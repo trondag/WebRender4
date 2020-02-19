e315bd3034a3b4c7002df1ca542764a307d41c00
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-02-19 09:50:43 +0100

    Add project files.

---
+++ WebRender4/Website.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+    //Microsoft.Extensions.Hosting.Abstractions
+
+namespace WebRender4
+{
+    public class Website
+    {
+        public Website(String[] args)
+        {
+            
+        }
+        public static void hei(String[] args)
+        {
+            Console.WriteLine("heeeeei" + args);
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
