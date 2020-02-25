3982a6c94e9c1788e9e20af38a877d3a7bec6fa8
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-02-25 09:54:46 +0100

    Laget mulighet for å starte localhost

--- WebRender4/Website.cs
+++ WebRender4/Website.cs
@@ -15,7 +15,7 @@ namespace WebRender4
         {
             
         }
-        public static void hei(String[] args)
+        public static void launch(String[] args)
         {
             Console.WriteLine("heeeeei" + args);
             CreateHostBuilder(args).Build().Run();
